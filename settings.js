f21ed417b8e5684ef45731248195b1f265a4c473
ZACK-BOTS <98458551+ZACK-BOTS@users.noreply.github.com>
2022-04-20 21:05:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,22 +18,22 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "𝙱𝙾𝚃 𝚅.1" //namabot kalian
-global.ownername= "𝒏𝒊𝒄𝒉𝒖 𝒗𝒅𝒓" //nama kalian
-global.myweb ="https://chat.whatsapp.com/CZ5k2wN4HslD62oiLth8TB" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/lltrker" //bebas asal jan hapus
-global.github = "https://Nichuvdr.github.io/" //bebas
-global.email = "pnichu786@gmail.com" //bebas
+global.botname = "Z4CK" //namabot kalian
+global.ownername= "Z4CK MODZ" //nama kalian
+global.myweb ="https://chat.whatsapp.com/G27ou2ubRj91aqADarCTN4 " //bebas asal jan hapus
+global.youtube = "https://youtube.com/channel/UCgpji1WYjJWaGwr0s8CEteQ" //bebas asal jan hapus
+global.github = "not available" //bebas
+global.email = "gmaail illa vro" //bebas
 global.region = "India" //bebas
 global.ownernomer = "919207980284" // nomor wa kalian
 global.ownernomerr = "+919207980284" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["919207980284","917994107442","919207980284"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© 𝙱𝙾𝚃 𝚅.1' //sticker wm ubah
-global.author = '𝐵𝑦 𝑁𝑖𝑐ℎ𝑢 𝑣𝑑𝑟' //sticker wm ganti nama kalian
-global.sessionName = '𝐿𝑖𝑣𝑒 𝑛𝑜𝑤'
+global.owner = ["919746245269","916282376660","916282376660"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© ZACK' //sticker wm ubah
+global.author = 'By Zack Modz' //sticker wm ganti nama kalian
+global.sessionName = 'Meyr'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
